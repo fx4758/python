98023c56e33d7521539ad8d964810a2dd8b6e8e4
wuweihua <175990271@qq.com>
2018-11-02 23:49:28 +0800

    1

--- baiduzhidao/bdzd.py
+++ baiduzhidao/bdzd.py
@@ -4,6 +4,7 @@ from urllib.parse import quote
 from requests_html import HTMLSession
 import re
 import ssl
+from langconv import *
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -12,26 +13,25 @@ def htmldown(url):
     html = HTMLSession().get(url).html
     return html
 
-#发布到wordpress模块，支持标题，内容，自定义字段
-def post_to_wp(title,content,field):
-    wp = Client('http://127.0.0.1/xmlrpc.php','admin','199011')  #办公室111
-    post = WordPressPost()
-    post.post_status = 'publish'
-    post.terms_names ={'category':['西藏旅游问答']}
+#简体转换为繁体
+def jt_to_ft(line):
+    line = Converter('zh-hant').convert(line)
+    line.encode('utf-8')
+    return line
 
-    post.title = title
-    post.content = content
-    post.custom_fields = field
 
-    post.id = wp.call(posts.NewPost(post))
-    print(str(title) + '\nhttp://localhost/?p=' + str(post.id) + '\n')
+wp = Client('https://tibetdreamtravel.wordpress.com/xmlrpc.php','aimlokr','910409hbHS')  #办公室111
+post = WordPressPost()
+post.post_status = 'publish'
+post.terms_names ={'category':['西藏旅游问答']}
 
 #页面单页解析
 def jiexi(url):
-    print('开始采集：'+str(url))
-    html = htmldown(url)
     try:
+        print('开始采集：'+str(url))
+        html = htmldown(url)
         title = html.find('span.ask-title')[0].text
+        title = jt_to_ft(title)
 
         content = html.find('.content .best-text')[0].html
         content = re.sub('<(d|s|h|/|p|a|li|ol|ul).*?>','',content)
@@ -41,6 +41,20 @@ def jiexi(url):
         content = re.sub(' +',' ',content)
         content = re.sub('<b[\s\S]*?>','\n\n',content)
         content = re.sub('(<img[\s\S]*?>)','\n\n\g<1>\n\n',content)
+        content = jt_to_ft(content)
+
+        pic_list = re.findall('src="(.*?)"',content)
+        for pic in pic_list:
+            date = {
+                'name':str(hash(pic))+'.jpeg',
+                'type':'image/jpeg',
+            }
+            date['bits'] = HTMLSession().get(pic).content
+            repson = wp.call(media.UploadFile(date))
+            content = re.sub(pic,repson['url'],content)
+            pic_id = []
+            pic_id.append(repson['id'])
+            print(repson['url'])
 
         field = []
         field.append({
@@ -48,14 +62,19 @@ def jiexi(url):
             'value':url,
         })
 
-        post_to_wp(title,content,field)
+        post.title = title
+        post.content = content
+        post.custom_fields = field
+
+        post.id = wp.call(posts.NewPost(post))
+        print(str(title) + '\nhttps://tibetdreamtravel.wordpress.com/?p=' + str(post.id) + '\n')
 
     except:
         print('出现异常，跳过。\n\n')
 
 #关键词转码为GBK编码，并生成转码后的列表
 def zhuanma():
-    yswords = ['西藏旅游','布达拉宫','林芝','拉萨','西藏','入藏函','外国人到西藏']
+    yswords = ['西藏旅游','布达拉宫','林芝','拉萨','西藏','入藏函','外国人到西藏','台湾人到西藏旅游','香港人到西藏','华人到西藏','入藏证','西藏边防证']
     zmwords = []
     for ysword in yswords:
         ysword = ysword.encode('GBK','replace')
