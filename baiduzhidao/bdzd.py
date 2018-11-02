22f878e3c6810d7da0def37fa42915efec2fead2
wuweihua <175990271@qq.com>
2018-11-02 22:09:06 +0800

    1

--- baiduzhidao/bdzd.py
+++ baiduzhidao/bdzd.py
@@ -4,45 +4,58 @@ from urllib.parse import quote
 from requests_html import HTMLSession
 import re
 import ssl
-from tomorrow import threads
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-wp = Client('http://127.0.0.1/xmlrpc.php','admin','111')
-post = WordPressPost()
-post.post_status = 'publish'
-post.terms_names ={'category':['西藏旅游问答']}
-
-
+#网页下载
 def htmldown(url):
     html = HTMLSession().get(url).html
     return html
 
-def jiexi(url):
-    html = htmldown(url)
-    title = html.find('span.ask-title')[0].text
-
-    content = html.find('.content .best-text')[0].html
-    content = re.sub('<(d|s|h|/|p|a).*?>','',content)
-    content = re.sub('展开全部','',content)
-    content = re.sub('\n+([\s\S]*?)','',content)
-    content = re.sub('(class|esrc)=".*?"','',content)
-    content = re.sub(' +',' ',content)
-    content = re.sub('\n','\n\n',content)
+#发布到wordpress模块，支持标题，内容，自定义字段
+def post_to_wp(title,content,field):
+    wp = Client('http://127.0.0.1/xmlrpc.php','admin','199011')  #办公室111
+    post = WordPressPost()
+    post.post_status = 'publish'
+    post.terms_names ={'category':['西藏旅游问答']}
 
     post.title = title
     post.content = content
-    post.custom_fields = []
-    post.custom_fields.append({
-        'key':'via',
-        'value':url,
-    })
+    post.custom_fields = field
 
     post.id = wp.call(posts.NewPost(post))
-    print(str(title)+'\nhttp://127.0.0.1/?p='+str(post.id)+'\n')
+    print(str(title) + '\nhttp://127.0.0.1/?p=' + str(post.id) + '\n')
 
+#页面单页解析
+def jiexi(url):
+    print('开始采集：'+str(url))
+    html = htmldown(url)
+    try:
+        title = html.find('span.ask-title')[0].text
+
+        content = html.find('.content .best-text')[0].html
+        content = re.sub('<(d|s|h|/|p|a|li|ol|ul).*?>','',content)
+        content = re.sub('展开全部','',content)
+        content = re.sub('\n+([\s\S]*?)','',content)
+        content = re.sub('(class|esrc)=".*?"','',content)
+        content = re.sub(' +',' ',content)
+        content = re.sub('<b[\s\S]*?>','\n\n',content)
+        content = re.sub('(<img[\s\S]*?>)','\n\n\g<1>\n\n',content)
+
+        field = []
+        field.append({
+            'key':'via',
+            'value':url,
+        })
+
+        post_to_wp(title,content,field)
+
+    except:
+        print('出现异常，跳过。\n\n')
+
+#关键词转码为GBK编码，并生成转码后的列表
 def zhuanma():
-    yswords = ['西藏旅游','布达拉宫','林芝']
+    yswords = ['西藏旅游','布达拉宫','林芝','拉萨','西藏','入藏函','外国人到西藏']
     zmwords = []
     for ysword in yswords:
         ysword = ysword.encode('GBK','replace')
@@ -50,6 +63,7 @@ def zhuanma():
         zmwords.append(ysword)
     return zmwords
 
+#生成列表页，从列表页中获取单页
 def run(keyword):
     max_page = 760
     x = 0
@@ -64,5 +78,6 @@ def run(keyword):
                 jiexi(url)
         x = x + 10
 
+#从转码后的列表中获取单独关键词，进行采集发布
 for keyword in zhuanma():
     run(keyword)
